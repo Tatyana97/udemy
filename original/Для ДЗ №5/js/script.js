52fe6bcab3d03715023e41e4bfd1ad5447027dfe
Tatyana <tanyusha.miranovich.97@mail.ru>
2020-03-23 23:18:59 +0200

    finish 6 task

--- original/Для ДЗ №5/js/script.js
+++ original/Для ДЗ №5/js/script.js
@@ -1,6 +1,9 @@
 let navMenu = document.querySelector(".menu"),
-    navMenuItem = document.querySelectorAll(".menu .menu-item");
-    // bodyImgBackground = document.getElementsByTagName("body");
+    navMenuItem = document.querySelectorAll(".menu .menu-item"),
+    changeTextTitle = document.querySelector("#title"),
+    promptforApple = document.querySelector("#prompt"),
+    removeAdvertising = document.querySelector(".adv");
+    
 
 
 let addFivePoint = document.createElement("li");
@@ -8,7 +11,13 @@ addFivePoint.innerHTML = "Пятый пункт";
 addFivePoint.classList.add("menu-item");
 navMenu.appendChild(addFivePoint);
 
+document.body.style.backgroundImage = "url('img/apple_true.jpg')"; 
 
+changeTextTitle.innerHTML = "Мы продаем только подлинную технику Apple";
 
-document.getElementsByTagName("body").style.backgroundImage = "url(../../../img/apple_true.jpg) no-repeat";
+removeAdvertising.remove();
+
+let yourOpinion = prompt("Ваше отношение к технике apple?");
+promptforApple.textContent = yourOpinion;
+
 
